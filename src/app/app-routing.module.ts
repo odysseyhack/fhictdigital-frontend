83d7d8d8c97ea186cbe194b61c4358d117dd1a42
Giovanni Koolhoven <giok.p@outllook.com>
2019-04-13 13:20:57 +0200

    add login module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
 
